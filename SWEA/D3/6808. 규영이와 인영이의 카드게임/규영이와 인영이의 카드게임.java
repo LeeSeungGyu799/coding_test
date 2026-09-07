3348be70f12b6584b74d80910b3178b5bd9edf36
LeeSeungGyu799 <80658183+LeeSeungGyu799@users.noreply.github.com>
2026-09-07 16:33:57 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 1,852 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,89 +1,92 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-/*
-사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
-*/
 class Solution {
-	static ArrayList<Integer> cardA;
-	static ArrayList<Integer> cardB;
-	static int win;
-	static int lose;
-	static boolean[] isUsed;
 
-	public static void main(String args[]) throws Exception {
-		/*
-		 * 아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다. 여러분이 작성한 코드를
-		 * 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후, 이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때
-		 * 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다. 따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		 * 단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-//System.setIn(new FileInputStream("res/input.txt"));
+    static int[] gyu = new int[9];
+    static int[] in = new int[9];
 
-		/*
-		 * 표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(bf.readLine());
-		/*
-		 * 여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		 */
+    static boolean[] used = new boolean[9];
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			cardA = new ArrayList<>();
-			cardB = new ArrayList<>();
-			String s = bf.readLine();
-			StringTokenizer st = new StringTokenizer(s);
+    static int win;
+    static int lose;
 
-			for (int i = 0; i < 9; i++) {
-				cardA.add(Integer.parseInt(st.nextToken()));
-			}
+    public static void main(String[] args) throws Exception {
 
-			for (int i = 1; i < 19; i++) {
-				if (cardA.contains(i))
-					continue;
-				else
-					cardB.add(i);
-			}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			isUsed = new boolean[9];
-			win = 0;
-			lose = 0;
-			dfs(0, 0, 0);
+        int T = Integer.parseInt(br.readLine());
 
-			System.out.println("#" + test_case + " " + win + " " + lose);
-		}
-	}
+        for (int tc = 1; tc <= T; tc++) {
 
-	public static void dfs(int scoreA, int scoreB, int useCount) {
-		if (useCount == 9) {
-			if (scoreA > scoreB)
-				win++;
-			else if (scoreA < scoreB)
-				lose++;
+            boolean[] cardCheck = new boolean[19];
 
-			return;
-		}
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int cardNum = useCount;
+            for (int i = 0; i < 9; i++) {
+                gyu[i] = Integer.parseInt(st.nextToken());
+                cardCheck[gyu[i]] = true;
+            }
 
-		for (int j = 0; j < 9; j++) {
-			if (isUsed[j])
-				continue;
-			
-			isUsed[j] = true;
-			if (cardA.get(cardNum) > cardB.get(j)) {
-				dfs(scoreA + cardA.get(cardNum) + cardB.get(j), scoreB, useCount + 1);
-			} else {
-				dfs(scoreA, scoreB + cardA.get(cardNum) + cardB.get(j), useCount + 1);
-			}
+            int idx = 0;
 
-			isUsed[j] = false;
-		}
-	}
+            for (int card = 1; card <= 18; card++) {
+                if (!cardCheck[card]) {
+                    in[idx++] = card;
+                }
+            }
 
+            used = new boolean[9];
+
+            win = 0;
+            lose = 0;
+
+            dfs(0, 0, 0);
+
+            System.out.println("#" + tc + " " + win + " " + lose);
+        }
+    }
+
+    static void dfs(int round, int gyuScore, int inScore) {
+
+        // 9라운드 종료
+        if (round == 9) {
+
+            if (gyuScore > inScore) {
+                win++;
+            } else if (gyuScore < inScore) {
+                lose++;
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < 9; i++) {
+
+            if (used[i]) {
+                continue;
+            }
+
+            used[i] = true;
+
+            int gyuCard = gyu[round];
+            int inCard = in[i];
+
+            if (gyuCard > inCard) {
+                dfs(
+                    round + 1,
+                    gyuScore + gyuCard + inCard,
+                    inScore
+                );
+            } else {
+                dfs(
+                    round + 1,
+                    gyuScore,
+                    inScore + gyuCard + inCard
+                );
+            }
+
+            used[i] = false;
+        }
+    }
 }
